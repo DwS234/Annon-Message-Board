818876c8718a82f977a33fe248357d9b6139c615
dawid gonisiewski <dawid@dawid.gonisiewski>
2018-07-29 13:14:50 +0200

    Added POST /api/threads/:board and /api/replies/:board

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
 'use strict';
 
@@ -14,15 +14,108 @@ var mongoose = require('mongoose');
 var CONNECTION_STRING = process.env.DB;
 
 mongoose.connect(CONNECTION_STRING, (err) => {
-  if(err)
+  if (err)
     console.log("Couldn't connect to db: " + err);
 });
 
-module.exports = function (app) {
-  
-  app.route('/api/threads/:board')
-  ;
-    
-  app.route('/api/replies/:board');
+//Models
+const Board = require('../models/Board');
+const Thread = require('../models/Thread');
+const Reply = require('../models/Reply');
 
+function createThreadAndSaveToBoard(threadText, delete_password, board) {
+  Thread.create({
+    text: threadText,
+    delete_password: delete_password
+  }, (err, createdThread) => {
+    if (err)
+      return err;
+    board.threads.push(createdThread._id);
+    board.save();
+  });
 };
+
+
+module.exports = function (app) {
+
+  app.route('/api/threads/:board')
+    .post((req, res) => {
+      const boardName = req.params.board;
+      const threadText = req.body.text;
+      const threadDeletePassword = req.body.delete_password;
+
+      if (threadText == null)
+        return res.status(400).send("Thread text is required");
+
+      if (threadDeletePassword == null)
+        return res.status(400).send("Thread delete_password is required");
+
+      Board.findOne({
+        name: boardName
+      }, (err, foundBoard) => {
+        if (err)
+          return res.status(400).send(err);
+
+        if (!foundBoard) {
+          Board.create({
+            name: boardName
+          }, (err, createdBoard) => {
+            if (err)
+              return res.status(400).send(err);
+
+            const onSaveThreadPotentialError = createThreadAndSaveToBoard(threadText, threadDeletePassword, createdBoard);
+            if (onSaveThreadPotentialError)
+              return res.status(400).send(err);
+
+            res.redirect("/b/" + boardName);
+          });
+        } else {
+          const onSaveThreadPotentialError = createThreadAndSaveToBoard(threadText, threadDeletePassword, foundBoard);
+          if (onSaveThreadPotentialError)
+            return res.status(400).send(err);
+
+          res.redirect("/b/" + boardName);
+        }
+      });
+    });
+
+  app.route('/api/replies/:board')
+    .post((req, res) => {
+      const boardName = req.params.board;
+      const replyText = req.body.text;
+      const replyDeletePasswrod = req.body.delete_password;
+      const thread_id = req.body.thread_id;
+
+      Board.findOne({
+        name: boardName
+      }).populate("threads").exec((err, foundBoard) => {
+        if (err)
+          return res.status(400).send(err);
+
+        if (!foundBoard)
+          return res.status(400).send("Specified board was not found");
+
+        const foundThread = foundBoard.threads.filter(thread => {
+          return thread._id == thread_id;
+        })[0];
+
+        if(!foundThread)
+          return res.status(400).send("Thread with given id was not found");
+
+        Reply.create({text: replyText, delete_password: replyDeletePasswrod}, (err, createdReply) => {
+          if (err)
+            return res.status(400).send(err);
+          if(!createdReply)
+            return res.status(400).send('Could not create Reply');
+
+          foundThread.replies.push(createdReply._id);
+          foundThread.bumped_on = createdReply.created_on;
+          foundThread.save();
+
+          
+          
+          res.redirect("/b/" + boardName + "/" + thread_id);
+        }); 
+      });
+    });
+}
